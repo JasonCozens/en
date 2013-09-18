c24e755384272dae62327f0d2f5dd97feb2f68e9
Jason Cozens <jason.cozens@computer.org>
2013-09-18 23:08:25 +0100

    Idea now inherits from object.

--- n6/i.py
+++ n6/i.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-class Idea:
+class Idea(object):
 
      O = 'I'
 
